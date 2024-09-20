c87b9c8d72134dd1e4a75049c27ba3a2a2e556f6
elizapimentel <elizapimentel@hotmail.com>
2024-09-20 20:04:39 -0300

    add workflow

--- src/db/typeOrm.migration-config.ts
+++ src/db/typeOrm.migration-config.ts
@@ -2,21 +2,27 @@ import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import { GameEntity } from './entities/game.entity';
 
-config();
+config();  // Carrega as variáveis de ambiente do .env
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.DB_URL,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USER,
+  password: String(process.env.DB_PASSWORD || ''),
+  database: process.env.DB_NAME,
+  schema: 'public',
   entities: [GameEntity],
   synchronize: false,
   migrations: [__dirname + '/migrations/*.ts'],
   migrationsRun: true,
+  logging: true,
 });
 
 AppDataSource.initialize()
 .then(() => {
-  console.log("Data Source has been initialized!")
+  console.log("Data Source has been initialized!");
 })
 .catch((err) => {
-  console.error("Error during Data Source initialization", err)
+  console.error("Error during Data Source initialization", err);
 });
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ config();
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
